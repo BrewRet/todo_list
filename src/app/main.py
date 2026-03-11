f8d462cd4bb477e03818345b3b2944ef2cbc02e5
BrewIl <dudefist@mail.ru>
2026-03-11 11:04:49 +0700

    dev: реализован crud

--- src/app/main.py
+++ src/app/main.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 tasks = []
 
 
-class PostTask(BaseModel):
+class RequestTask(BaseModel):
     title: str
     description: str
 
@@ -37,6 +37,27 @@ async def get_task_by_id(id:int):
 
 
 @app.post("/tasks")
-async def post_task(task: PostTask):
-    pass
+async def post_task(task: RequestTask):
+    global tasks
+    tasks.append((task.title, task.description))
+    return {"result": "Task added"}
 
+
+@app.put("/tasks/{id}")
+async def change_task(id:int, task: RequestTask):
+    global tasks
+    try: 
+        tasks[id] = (task.title, task.description)
+    except IndexError:
+        raise HTTPException(404, f"Task by this id: {id}, not found")
+    return {f"task {id}": task[id]}
+
+
+@app.delete("/tasks/{id}")
+async def delete_task(id):
+    global tasks
+    try:
+        tasks.pop(id)
+    except IndexError:
+        raise HTTPException(404, f"Task by this id: {id}, not found")
+    return {"result": "Task deleted"}
